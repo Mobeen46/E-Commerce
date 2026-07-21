d7a76390a13700b0e0af315b50ebe10281860969
Mobeen46 <kalidindimobeen@gmail.com>
2026-07-21 22:50:16 +0530

    Deleted Abstraction files

--- Abstraction/SBI_child1.java
+++
@@ -1,9 +0,0 @@
-package Abstraction;
-
-public class SBI_child1 extends RBI_parent {
-	public void intrest()
-	{
-		System.out.println("SbI");
-	}
-
-}
--- com/java/mobeen/Actual_Expected.java
+++
@@ -1,9 +0,0 @@
-package com.java.mobeen;
-
-import org.openqa.selenium.WebDriver;
-
-public class Actual_Expected {
-WebDriver driver;
-
-
-}
